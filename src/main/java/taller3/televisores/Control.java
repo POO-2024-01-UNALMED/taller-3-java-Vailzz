6c038c4fc4e3aae36a02bb14901e105d29fbc8b5
Juan Villa <jujotovil@gmail.com>
2024-02-26 22:39:57 -0500

    Seven commit

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,50 +1,39 @@
 package taller3.televisores;
+public class Control{
+    private TV tv;
 
-public class Control {
-	private TV tv;
-	
-	public void turnOn() {
-		tv.turnOn();
-	}
-	
-	public void turnOff() {
-		tv.turnOff();
-	}
-	
-	public void canalUp() {
-		tv.canalUp();
-	}
-	
-	public void canalDown() {
-		tv.canalDown();
-	}
-	
-	public void volumenUp() {
-		tv.volumenUp();
-	}
-	
-	public void volumenDown() {
-		tv.volumenDown();
-	}
-	
-	public void setCanal() {
-		tv.setCanal(0);
-	}
-	
-	public void setVolumen() {
-		tv.setVolumen(0);
-	}
-	
+    public void turnOn(){
+        tv.turnOn();
+    }
+    public void turnOff(){
+        tv.turnOff();
+    }
+    public void canalUp(){
+        tv.canalUp();
+    }
+    public void canalDown(){
+        tv.canalDown();
+    }
+    public void volumenUp(){
+        tv.volumenUp();
+    }
+    public void volumenDown(){
+        tv.volumenDown();
+    }
+    public void setCanal(int nuevoCanal){
+        tv.setCanal(nuevoCanal);
+    }
+    public void setVolumen(int nuevoVolumen){
+        tv.setVolumen(nuevoVolumen);
+    }
     public void enlazar(TV televisor){
         tv=televisor;
         tv.setControl(this);
     }
-	
-	public TV getTv() {
-		return this.tv;
-	}
-	
-	public void setTv(TV tv) {
-		this.tv = tv;
-	}
+    public TV getTv(){
+    return tv;
+    }
+    public void setTv(TV tv){
+        this.tv = tv;
+    }
 }
