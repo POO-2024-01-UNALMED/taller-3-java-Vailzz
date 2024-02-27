88734ea7d8230a16ee7ce65929fdf957653c6f4e
Juan Villa <jujotovil@gmail.com>
2024-02-26 22:30:35 -0500

    Second commit

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -1,5 +1,109 @@
 package taller3.televisores;
 
 public class TV {
+	private Marca marca;
+	private int canal;
+	private int precio;
+	private boolean estado;
+	private int volumen;
+	private Control control;
+	
+	public static int numTV = 0;
+	
+	public TV(Marca marca, boolean estado) {
+		this.marca = marca;
+		this.estado = estado;
+		this.canal = 1;
+		this.volumen = 1;
+		this.precio = 500;
+		TV.numTV++;
+	}
+	
+// GETTERS Y SETTERS
+	public Marca getMarca() {
+		return this.marca;
+	}
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+	
+	
+	public int getCanal() {
+		return this.canal;
+	}
+	public void setCanal(int canal) {
+		this.canal = canal;
+	}
+	
+	
+	public int getPrecio() {
+		return this.precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+	
+	
+	public int getVolumen() {
+		return this.volumen;
+	}
+	public void setVolumen(int volumen) {
+		this.volumen = volumen;
+	}
+	
+	
+	public Control getControl() {
+		return this.control;
+	}
+	public void setControl(Control control) {
+		this.control = control;
+	}
+	
+	
+	public boolean getEstado() {
+		return this.estado;
+	}
+	
+
+	
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+//Métodos TurnOn, turnOFF, canalUp, canalDown, volumenUp, volumenDown
+	
+	public boolean turnOn() {
+		return this.estado = true;
+	}
+	public boolean turnOff() {
+		return this.estado = false;
+	}
+	
+	public void canalUp() {
+		if (this.estado && this.canal < 120) {
+			this.canal++;
+		}
+	}
+	
+	public void canalDown() {
+		if (this.estado && this.canal > 1) {
+			this.canal++;
+		}
+	}
+	
+	public void volumenUp() {
+		if (this.estado == true && this.volumen < 7) {
+			this.volumen++;
+		}
+	}
+	
+	public void volumenDown() {
+		if(this.estado == true && this.volumen > 0) {
+			this.volumen++;
+		}
+	}
+	
+	
+	
+	
+
 
 }
